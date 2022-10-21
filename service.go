1edf6dafafca57067fa8e033120f9d90de7d901d
Oto Adamia <oadamia@gmail.com>
2022-10-21 16:59:39 +0400

    set e.Validator

--- service.go
+++ service.go
@@ -1,9 +1,13 @@
 package service
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/oadamia/service/validator"
+)
 
 // New creates and returns service instance
 func New() (e *echo.Echo) {
 	e = echo.New()
+	e.Validator = validator.New()
 	return
 }
