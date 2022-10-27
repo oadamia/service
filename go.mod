6ebea02068ecff4fc67b09ce1c06dd64763573f1
Oto Adamia <oadamia@gmail.com>
2022-10-27 17:35:47 +0400

    add UseJWTMiddleware function

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -22,5 +23,6 @@ require (
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- service.go
+++ service.go
@@ -2,14 +2,27 @@ package service
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/oadamia/logwrapper"
 	"github.com/oadamia/service/validator"
 )
 
+var authSecret string
+
 // New creates and returns service instance
-func New() (e *echo.Echo) {
+func New(secret string) (e *echo.Echo) {
 	e = echo.New()
 	e.Validator = validator.New()
 	e.Logger = logwrapper.Wrapper()
+	authSecret = secret
 	return
+}
+
+// UseJWTMidddleware set JWT middleware for group
+func UseJWTMidddleware(g *echo.Group) {
+	jwtConfig := middleware.JWTConfig{
+		// Claims:     &jwt.UserClaim{},
+		SigningKey: []byte(authSecret),
+	}
+	g.Use(middleware.JWTWithConfig(jwtConfig))
 }
